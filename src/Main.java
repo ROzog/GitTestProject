137d70123aa26783bd4dd78f31bffa81ee61efa6
test <asd@asd.pl>
2018-07-17 13:47:04 +0200

    fjkdshb dsvzjkhkujh

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("bvjhdahysfaujyabf");
         System.out.println("coz za piekny tekst :P");
+        System.out.println("Wiem :D");
     }
 }
