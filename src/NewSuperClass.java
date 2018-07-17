c7051f83c65521b3b00542ac519522766ad86035
ROzog <info@robertozog.com>
2018-07-17 13:51:08 +0200

    without pull before

--- src/NewSuperClass.java
+++ src/NewSuperClass.java
@@ -1,5 +1,10 @@
 public class NewSuperClass {
     public static void main(String[] args) {
         System.out.println("nowy komunikat");
+
+        int a=7;
+        int b=9;
+
+        System.out.println(a+b);
     }
 }
